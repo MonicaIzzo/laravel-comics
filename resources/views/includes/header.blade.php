6a5ca42b8eb9fe2c1f8f844c290de854310ad8b2
MonicaIzzo <info@monicaizzo.net>
2023-09-04 05:50:24 +0200

    ADD Layout HEADER

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,1 +1,28 @@
-HEADER
+<header>
+    <div class="header-top container">
+        <div class="content">
+            <div class="text">DC POWER VISA</div>
+            <div class="text">ADDITIONAL DC SITE</div>
+        </div>
+
+
+    </div>
+    <nav class="container">
+        <img class="logo" src={{ Vite::asset('resources/img/dc-logo.png') }} alt="Logo DC">
+        </div>
+        <div>
+            <ul>
+                <li><a href={{ route('characters') }} class="" alt="">CHARACTERS</a></li>
+                <li><a href={{ route('comics') }} class="" alt="">COMICS</a></li>
+                <li><a href={{ route('movies') }} class="" alt="">MOVIES</a></li>
+                <li><a href={{ route('tv') }} class="" alt="">TV</a></li>
+                <li><a href={{ route('games') }} class="" alt="">GAMES</a></li>
+                <li><a href={{ route('collectibles') }} class="" alt="">COLLECTIBLES</a></li>
+                <li><a href={{ route('videos') }} class="" alt="">VIDEOS</a></li>
+                <li><a href={{ route('fans') }} class="" alt="">FANS</a></li>
+                <li><a href={{ route('news') }} class="" alt="">NEWS</a></li>
+                <li><a href={{ route('shop') }} class="" alt="">SHOP</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
